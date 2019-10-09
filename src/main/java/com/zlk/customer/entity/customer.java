4feee73250a91b7b555360aebe9e5e1c30e04b47
yzh <yzhsteven@qq.com>
2019-10-09 19:55:18 +0800

    yzh

---
+++ src/main/java/com/zlk/customer/entity/customer.java
@@ -0,0 +1,13 @@
+package com.zlk.customer.entity;
+
+/**
+ * @ClassName： customer
+ * @Description：
+ * @Author： yzh
+ * @Date： 2019/10/9 19:54
+ */
+
+public class customer {
+    private Integer id;
+
+}
